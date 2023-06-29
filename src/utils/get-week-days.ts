be05b6f69da6501a6e67c762cc14aeb4248ca1bb
Andre Possiedi <andreefps@gmail.com>
2023-06-29 20:58:01 +0200

    feat: calendar page layout

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,9 +1,11 @@
-export function getWeekDays() {
+export function getWeekDays(short = false) {
   const formatter = new Intl.DateTimeFormat('en-US', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 10, day))))
     .map((weekDay) => {
-      return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
+      return short
+        ? weekDay.substring(0, 3).toUpperCase()
+        : weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
     })
 }
